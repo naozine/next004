f8220df0be98aa0b1ec8b534a843f5afa2c4224a
naozine <naozine@outlook.jp>
2021-10-23 15:15:41 +0900

    add jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@chakra-ui/layout'
 import { chakra } from '@chakra-ui/system'
+import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      <Box>
-        <chakra.h1 color="tomato">Hello Chakra!</chakra.h1>
-      </Box>
-    </div>
+    <Box>
+      <chakra.h1 color="tomato">Hello Chakra!</chakra.h1>
+    </Box>
+    // <h1>Hello Jest!</h1>
   )
 }
