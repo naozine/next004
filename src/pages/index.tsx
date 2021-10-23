1fd1043a1d896ecd7a8623f9a2de028fe82cc15b
naozine <naozine@outlook.jp>
2021-10-23 13:28:14 +0900

    add chakra ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Box } from '@chakra-ui/layout'
+import { chakra } from '@chakra-ui/system'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello vercel!</h1>
+      <Box>
+        <chakra.h1 color="tomato">Hello Chakra!</chakra.h1>
+      </Box>
     </div>
-  );
+  )
 }
